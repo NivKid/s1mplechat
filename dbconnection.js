82bc9092517eb2a1d5da35da770880ad1c95fe1e
NivKid <supernived619@gmail.com>
2017-11-23 11:21:03 +0530

    db new migration fix

--- dbconnection.js
+++ dbconnection.js
@@ -4,13 +4,11 @@ const initOptions = {
 };
 const pgp = require('pg-promise')(initOptions);
 const cn = {
-    host: 'ec2-54-83-19-82.compute-1.amazonaws.com
-',
+    host: 'ec2-54-83-19-82.compute-1.amazonaws.com',
     port: 5432,
     database: 'd9efj0lcoahrju',
     user: 'iwzqtxfysglrbl',
-    password: '97432d26d1dcab1c15ab5f1b45f02abbe9e3fe7e24c97b8b3df261da601ebe3a
-'
+    password: '97432d26d1dcab1c15ab5f1b45f02abbe9e3fe7e24c97b8b3df261da601ebe3a'
 };
 
 // const localdbConnection = {
